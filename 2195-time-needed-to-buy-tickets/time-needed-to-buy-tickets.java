5265322c390e7d43690b0ac2fdfb768aded71b96
Nihal Yadav <118679879+yadavnihal@users.noreply.github.com>
2026-01-05 01:06:07 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (57.35%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -1,18 +1,30 @@
 class Solution {
     public int timeRequiredToBuy(int[] arr, int k) {
-        int c=0;
-        int i=0;
-        while(arr[k]!=0){
-           if(i==arr.length) i=0;     
-            while(arr[i]==0) {
-                i++;
-        if(i==arr.length) i=0;
-            }
-arr[i]=arr[i]-1;
-i++;
+        // int c = 0;
+        // int i = 0;
+        // while (arr[k] != 0) {
+        //     if (i == arr.length)
+        //         i = 0;
+        //     while (arr[i] == 0) {
+        //         i++;
+        //         if (i == arr.length)
+        //             i = 0;
+        //     }
+        //     arr[i] = arr[i] - 1;
+        //     i++;
 
-c++;
+        //     c++;
+        // }
+        // return c;
+        int t=0;
+        for(int i=0;i<arr.length;i++){
+            if(i<=k){
+                t+=Math.min(arr[i],arr[k]);
+            }else{
+                t+=Math.min(arr[i],arr[k]-1);
+
+            }
         }
-        return c;
+        return t;
     }
 }
